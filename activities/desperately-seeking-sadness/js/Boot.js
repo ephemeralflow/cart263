de77d0d21d2964c86ef21b42a89cb00c19212596
ephemeralflow <144470618+ephemeralflow@users.noreply.github.com>
2024-03-03 18:40:58 -0500

    done the seeking of sadness activity

--- activities/desperately-seeking-sadness/js/Boot.js
+++ activities/desperately-seeking-sadness/js/Boot.js
@@ -7,6 +7,9 @@ class Boot extends Phaser.Scene {
 
     preload() {
         //Loading Assets
+        this.load.image("avatar", "assets/images/avatar.png")
+        this.load.image("thumbs-down", "assets/images/thumbs-down.png")
+        this.load.image("thumbs-up", "assets/images/thumbs-up.png")
 
         this.load.on("complete", () => {
             this.scene.start("play");
--- activities/desperately-seeking-sadness/js/main.js
+++ activities/desperately-seeking-sadness/js/main.js
@@ -1,11 +1,20 @@
 /**
-Title of Project
+Sad and All That Jazz (Desperately Seeking Sadness) 
 Author Name
 
-This is a template. You must fill in the title,
-author, and this description to match your project!
+some sad guy going hm i stay sad i hate :DDD things yeah
 */
 
 "use strict";
 
-// Code goes here
+let config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    physics: {
+        default: "arcade"
+    },
+    scene: [Boot, Play]
+}
+
+let game = new Phaser.Game(config)
